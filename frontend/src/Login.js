f06e98fbe64b5d77192004ab43762272b909a0a5
blentsegaye49-cpu <blentsegaye49@gmail.com>
2026-09-09 01:29:49 -0900

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -29,7 +29,7 @@ const Login = () => {
       const payload = mode === 'login'
         ? { username, password }
         : { username, email, password, role }
-      const response = await fetch(`http://localhost:5000${endpoint}`, {
+      const response = await fetch("https://ehtionexa.onrender.com"`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload)
