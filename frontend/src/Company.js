9bbcddfffe42b541c32208f05235192aaa842169
blentsegaye49-cpu <blentsegaye49@gmail.com>
2026-09-09 00:33:42 -0900

    Update Company.js

--- frontend/src/Company.js
+++ frontend/src/Company.js
@@ -42,7 +42,7 @@ const Company = () => {
     setIsSubmitting(true);
     const company = { CompanyName, Phonenumber, EmailAddress, Companylocation, Companytype, WorkField, Numberofemployees, Companydiscription };
 
-    fetch('http://localhost:5000/Companies/', {
+    fetch(https://ehtionexa.onrender.com, {
       method: 'POST',
       headers: { "Content-Type": "application/json", Authorization: authUser.token },
       body: JSON.stringify(company)
